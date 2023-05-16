fc3100ce39ea3f4ebf9cd63fc36da995ddb4f0e1
Preston Evans <preston.b.evans@gmail.com>
2023-05-16 13:34:11 -0500

    Store receipts in ledger_db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use demo_app::app::create_demo_config;
+use demo_app::app::create_demo_genesis_config;
 use demo_app::app::{DefaultPrivateKey, NativeAppRunner};
 use jupiter::da_service::{CelestiaService, DaServiceConfig};
 use jupiter::types::NamespaceId;
 use jupiter::verifier::CelestiaVerifier;
 use jupiter::verifier::RollupParams;
 use risc0_adapter::host::Risc0Host;
-use sovereign_db::ledger_db::LedgerDB;
+use sovereign_db::ledger_db::{LedgerDB, SlotCommit};
 use sovereign_sdk::da::DaVerifier;
 use sovereign_sdk::services::da::DaService;
 use sovereign_sdk::stf::{StateTransitionFunction, StateTransitionRunner};
@@ -38,9 +38,15 @@ async fn main() -> Result<(), anyhow::Error> {
 
     // Initialize the Celestia service
     let mut demo_runner = NativeAppRunner::<Risc0Host>::new(DATA_DIR_LOCATION);
+    let celestia_rpc_auth_token = if !CELESTIA_NODE_AUTH_TOKEN.is_empty() {
+        CELESTIA_NODE_AUTH_TOKEN.to_string()
+    } else {
+        std::env::var("CELESTIA_NODE_AUTH_TOKEN")
+            .expect("please set CELESTIA_NODE_AUTH_TOKEN environment variable")
+    };
     let da_service = CelestiaService::new(
         DaServiceConfig {
-            celestia_rpc_auth_token: CELESTIA_NODE_AUTH_TOKEN.to_string(),
+            celestia_rpc_auth_token,
             celestia_rpc_address: "http://localhost:11111/".into(),
             max_celestia_response_body_size: 1024 * 1024 * 100,
         },
@@ -54,18 +60,36 @@ async fn main() -> Result<(), anyhow::Error> {
 
     // Initialize the demo app
     let demo = demo_runner.inner_mut();
-    let genesis_config = create_demo_config(
+    let sequencer_private_key = DefaultPrivateKey::generate();
+    let genesis_config = create_demo_genesis_config(
         100000000,
-        &DefaultPrivateKey::generate(),
-        &DefaultPrivateKey::generate(),
+        sequencer_private_key.default_address::<sha2::Sha256>(),
+        vec![
+            99, 101, 108, 101, 115, 116, 105, 97, 49, 122, 102, 118, 114, 114, 102, 97, 113, 57,
+            117, 100, 54, 103, 57, 116, 52, 107, 122, 109, 115, 108, 112, 102, 50, 52, 121, 115,
+            97, 120, 113, 102, 110, 122, 101, 101, 53, 119, 57,
+        ],
+        &sequencer_private_key,
+        &sequencer_private_key,
     );
-    demo.init_chain(genesis_config);
+
+    let item_numbers = ledger_db.get_next_items_numbers();
+    let last_slot_processed_before_shutdown = item_numbers.slot_number - 1;
+    if last_slot_processed_before_shutdown == 0 {
+        print!("No history detected. Initializing chain...");
+        demo.init_chain(genesis_config);
+        println!("Done.");
+    } else {
+        println!("Chain is already initialized. Skipping initialization.");
+    }
+
     demo.begin_slot(Default::default());
     let (prev_state_root, _, _) = demo.end_slot();
     let mut prev_state_root = prev_state_root.0;
 
+    let start_height = START_HEIGHT + last_slot_processed_before_shutdown;
     // Request data from the DA layer and apply it to the demo app
-    for height in START_HEIGHT.. {
+    for height in start_height..start_height + 10 {
         println!(
             "Requesting data for height {} and prev_state_root 0x{}",
             height,
@@ -74,20 +98,20 @@ async fn main() -> Result<(), anyhow::Error> {
         let filtered_block = da_service.get_finalized_at(height).await?;
         let header = filtered_block.header.clone();
         let (blob_txs, inclusion_proof, completeness_proof) =
-            da_service.extract_relevant_txs_with_proof(filtered_block);
+            da_service.extract_relevant_txs_with_proof(filtered_block.clone());
         assert!(da_verifier
             .verify_relevant_tx_list(&header, &blob_txs, inclusion_proof, completeness_proof)
             .is_ok());
         println!("Received {} blobs", blob_txs.len());
 
+        let mut data_to_commit = SlotCommit::new(filtered_block);
         demo.begin_slot(Default::default());
         for blob in blob_txs.clone() {
             let receipts = demo.apply_blob(blob, None);
-            for receipt in receipts {
-                l
-            }
+            data_to_commit.add_batch(receipts);
         }
         let (next_state_root, _witness, _) = demo.end_slot();
+        ledger_db.commit_slot(data_to_commit)?;
         prev_state_root = next_state_root.0;
     }
 
