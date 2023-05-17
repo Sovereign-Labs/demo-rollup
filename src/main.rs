c3d530b451004b6234867fcf7427e019f625945d
Nikolai Golub <nikolay.v.golub@gmail.com>
2023-05-17 19:45:32 +0200

    Fix/pre alpha cleanup (#10)
    * It  compiles
    
    * Clean up moved data
    
    * Cleanup; remove unused code. Add da verification
    
    * WIP: add storage + RPC
    
    * Store receipts in ledger_db
    
    * Adding Rollup Config and reading it from TOML (#12)
    
    * Create rollup config
    
    * Revert namespace id
    
    * Cleaning up more
    
    * Update config sample
    
    * Uncomment things
    
    ---------
    
    Co-authored-by: Preston Evans <preston.b.evans@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,101 @@
-use crate::runtime::GenesisConfig;
-use bank::TokenConfig;
-use jsonrpsee::http_client::HeaderMap;
-use jupiter::da_service::{CelestiaService, FilteredCelestiaBlock};
-use sha2::{Digest, Sha256};
-use sov_app_template::{AppTemplate, Batch};
-use sov_modules_api::{mocks::MockContext, Address};
-use sov_state::ProverStorage;
-use sovereign_db::{
-    ledger_db::{LedgerDB, SlotCommitBuilder},
-    schema::types::{
-        BatchNumber, DbBytes, EventNumber, Status, StoredBatch, StoredSlot, StoredTransaction,
-        TxNumber,
-    },
-};
-use sovereign_sdk::serial::Decode;
-use sovereign_sdk::services::da::SlotData;
-use sovereign_sdk::{da::BlobTransactionTrait, stf::StateTransitionFunction};
-use sovereign_sdk::{serial::Encode, services::da::DaService};
+mod config;
 
+use crate::config::RollupConfig;
+use demo_app::app::create_demo_config;
+use demo_app::app::{DefaultPrivateKey, NativeAppRunner};
+use demo_app::config::from_toml_path;
+use jupiter::da_service::CelestiaService;
+use jupiter::types::NamespaceId;
+use jupiter::verifier::CelestiaVerifier;
+use jupiter::verifier::RollupParams;
+use risc0_adapter::host::Risc0Host;
+use sovereign_db::ledger_db::{LedgerDB, SlotCommit};
+use sovereign_sdk::da::DaVerifier;
+use sovereign_sdk::services::da::DaService;
+use sovereign_sdk::stf::{StateTransitionFunction, StateTransitionRunner};
 use tracing::Level;
-use tx_verifier_impl::DemoAppTxVerifier;
 
-use crate::{runtime::Runtime, tx_hooks_impl::DemoAppTxHooks};
-
-mod data_generation;
-mod helpers;
-mod rpc;
-mod runtime;
-mod tx_hooks_impl;
-mod tx_verifier_impl;
-
-type C = MockContext;
-type DemoApp =
-    AppTemplate<C, DemoAppTxVerifier<C>, Runtime<C>, DemoAppTxHooks<C>, GenesisConfig<C>>;
-const CELESTIA_NODE_AUTH_TOKEN: &'static str = "";
-
-const START_HEIGHT: u64 = HEIGHT_OF_FIRST_TXS - 5;
 // I sent 8 demo election transactions at height 293686, generated using the demo app data generator
-const HEIGHT_OF_FIRST_TXS: u64 = 293686;
-pub fn default_celestia_service() -> CelestiaService {
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        "Authorization",
-        format!("Bearer {}", CELESTIA_NODE_AUTH_TOKEN)
-            .parse()
-            .unwrap(),
-    );
-    let client = jsonrpsee::http_client::HttpClientBuilder::default()
-        .set_headers(headers)
-        .max_request_body_size(1024 * 1024 * 100) // 100 MB
-        .build("http://localhost:11111/")
-        .unwrap();
-    CelestiaService::with_client(client)
-}
+const DATA_DIR_LOCATION: &str = "demo_data";
+const ROLLUP_NAMESPACE: NamespaceId = NamespaceId([115, 111, 118, 45, 116, 101, 115, 116]);
 
-fn sha2(data: impl AsRef<[u8]>) -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    hasher.update(data.as_ref());
-    hasher.finalize().into()
+pub fn initialize_ledger() -> LedgerDB {
+    let ledger_db = LedgerDB::with_path(DATA_DIR_LOCATION).expect("Ledger DB failed to open");
+    ledger_db
 }
-
-const DATA_DIR_LOCATION: &'static str = "demo_data";
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let sequencer_address: Address = [1u8; 32].into();
+    let rollup_config: RollupConfig = from_toml_path("rollup_config.toml")?;
 
+    // Initializing logging
     let subscriber = tracing_subscriber::fmt()
         .with_max_level(Level::WARN)
         .finish();
     tracing::subscriber::set_global_default(subscriber)
         .map_err(|_err| eprintln!("Unable to set global default subscriber"))
         .expect("Cannot fail to set subscriber");
-    let cel_service = default_celestia_service();
-    let ledger_db = LedgerDB::with_path(DATA_DIR_LOCATION).unwrap();
-    let storage = ProverStorage::with_path(DATA_DIR_LOCATION).unwrap();
-    let mut demo = DemoApp::new(
-        storage.clone(),
-        Runtime::new(),
-        DemoAppTxVerifier::new(),
-        DemoAppTxHooks::new(),
-        GenesisConfig::new(
-            (),
-            bank::BankConfig {
-                tokens: vec![TokenConfig {
-                    token_name: "sovereign".to_string(),
-                    address_and_balances: vec![(sequencer_address, 1000)],
-                }],
-            },
-            accounts::AccountConfig { pub_keys: vec![] },
-        ),
+
+    let ledger_db = initialize_ledger();
+
+    // Initialize the Celestia service
+    let mut demo_runner = NativeAppRunner::<Risc0Host>::new(rollup_config.runner.clone());
+    let da_service = CelestiaService::new(
+        rollup_config.da.clone(),
+        RollupParams {
+            namespace: ROLLUP_NAMESPACE,
+        },
     );
+    let da_verifier = CelestiaVerifier::new(RollupParams {
+        namespace: ROLLUP_NAMESPACE,
+    });
 
-    let rpc_ledger = ledger_db.clone();
-    let rpc_storage = storage.clone();
+    // Initialize the demo app
+    let demo = demo_runner.inner_mut();
+    let sequencer_private_key = DefaultPrivateKey::generate();
+    let genesis_config =
+        create_demo_config(100000000, &sequencer_private_key, &sequencer_private_key);
 
-    let _rpc_handle = rpc::RpcProvider::start(rpc_ledger, rpc_storage).await?;
-
-    let mut item_numbers = ledger_db.get_next_items_numbers();
-    if item_numbers.slot_number == 1 {
+    let item_numbers = ledger_db.get_next_items_numbers();
+    let last_slot_processed_before_shutdown = item_numbers.slot_number - 1;
+    if last_slot_processed_before_shutdown == 0 {
         print!("No history detected. Initializing chain...");
-        demo.init_chain(());
+        demo.init_chain(genesis_config);
         println!("Done.");
     } else {
         println!("Chain is already initialized. Skipping initialization.");
     }
-    let last_slot_processed_before_shutdown = item_numbers.slot_number - 1;
 
-    println!("Beginning sync from da slot {}...", START_HEIGHT);
-    for i in 0.. {
-        let height = START_HEIGHT + i;
-        if last_slot_processed_before_shutdown > i {
-            continue;
+    demo.begin_slot(Default::default());
+    let (prev_state_root, _, _) = demo.end_slot();
+    let mut prev_state_root = prev_state_root.0;
+
+    let start_height = rollup_config.start_height + last_slot_processed_before_shutdown;
+    // Request data from the DA layer and apply it to the demo app
+    for height in start_height..start_height + 10 {
+        println!(
+            "Requesting data for height {} and prev_state_root 0x{}",
+            height,
+            hex::encode(&prev_state_root)
+        );
+        let filtered_block = da_service.get_finalized_at(height).await?;
+        let header = filtered_block.header.clone();
+        let (blob_txs, inclusion_proof, completeness_proof) =
+            da_service.extract_relevant_txs_with_proof(filtered_block.clone());
+        assert!(da_verifier
+            .verify_relevant_tx_list(&header, &blob_txs, inclusion_proof, completeness_proof)
+            .is_ok());
+        println!("Received {} blobs", blob_txs.len());
+
+        let mut data_to_commit = SlotCommit::new(filtered_block);
+        demo.begin_slot(Default::default());
+        for blob in blob_txs.clone() {
+            let receipts = demo.apply_blob(blob, None);
+            data_to_commit.add_batch(receipts);
         }
-
-        let filtered_block: FilteredCelestiaBlock = cel_service.get_finalized_at(height).await?;
-        let slot_hash = filtered_block.hash();
-        let mut data_to_persist = SlotCommitBuilder::default();
-        let batches = cel_service.extract_relevant_txs(filtered_block);
-
-        demo.begin_slot();
-        let num_batches = batches.len();
-        for raw_batch in batches {
-            let mut data = raw_batch.data();
-            let batch = match Batch::decode(&mut data) {
-                Ok(batch) => batch,
-                Err(e) => {
-                    println!("    Error decoding batch: {}. Skipping.", e);
-                    continue;
-                }
-            };
-            let batch_hash = sha2(batch.encode_to_vec());
-            let tx_start = item_numbers.tx_number;
-            let num_txs = batch.txs.len();
-            let mut batch_to_store = StoredBatch {
-                sender: raw_batch.sender.as_ref().to_vec(),
-                hash: batch_hash,
-                extra_data: DbBytes::new(raw_batch.sender.as_ref().to_vec()),
-                txs: TxNumber(tx_start)..TxNumber(tx_start + num_txs as u64),
-                status: Status::Skipped,
-            };
-            item_numbers.tx_number += num_txs as u64;
-            print!("    Applying batch of {} transactions...", num_txs);
-
-            match demo.apply_batch(batch.clone(), raw_batch.sender().as_ref(), None) {
-                Ok(events) => {
-                    println!(" Done!");
-                    batch_to_store.status = Status::Applied;
-                    data_to_persist.txs.extend(batch.txs.into_iter().map(|tx| {
-                        let start_event_number = item_numbers.event_number;
-                        let end_event_number = start_event_number + events.len() as u64;
-                        item_numbers.event_number = end_event_number;
-                        StoredTransaction {
-                            hash: sha2(&tx.data[..]),
-                            events: EventNumber(start_event_number)..EventNumber(end_event_number),
-                            data: DbBytes::new(tx.data),
-                            status: Status::Applied,
-                        }
-                    }));
-                    data_to_persist.batches.push(batch_to_store);
-
-                    data_to_persist.events.extend(events.into_iter());
-                }
-                Err(e) => {
-                    println!(
-                        " Uh-oh! Failed to apply batch. Applying consensus set update {:?}",
-                        e
-                    );
-                }
-            }
-        }
-
-        demo.end_slot();
-        data_to_persist.slot_data = Some(StoredSlot {
-            hash: slot_hash,
-            extra_data: DbBytes::new(vec![]),
-            batches: BatchNumber(item_numbers.batch_number)
-                ..BatchNumber(item_numbers.batch_number + num_batches as u64),
-        });
-        item_numbers.batch_number += num_batches as u64;
-        ledger_db.commit_slot(data_to_persist.finalize()?)?;
+        let (next_state_root, _witness, _) = demo.end_slot();
+        ledger_db.commit_slot(data_to_commit)?;
+        prev_state_root = next_state_root.0;
     }
 
     Ok(())
